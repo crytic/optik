bcace38e30579c4606897361a444529b6eb8b12a
Boyan MILANOV <boyan.milanov@trailofbits.com>
2022-06-04 18:28:34 +0200

    Full hybrid fuzzing script (#13)
    * Basic echidna integration for a single fuzz campaign
    
    * Run multiple fuzzing campaign iterations
    
    * Replay only new corpus files at each symex iteration
    
    * Update README
    
    * Don't spend time solving different inputs to the same target
    
    * Update README
    
    * Support more echidna options
    
    * Run all transactions in sequence, add warning about sequences of multiple transactions not being supported
    
    * Temp file formatting for contract binaries
    
    * linting
    
    Co-authored-by: Tom Malcolm <ThomasMalcolm01@gmail.com>

--- optik/echidna/__main__.py
+++ optik/echidna/__main__.py
@@ -1,35 +1,177 @@
 import argparse
 import sys
+import os
 
-from .runner import replay_inputs, generate_new_inputs
+from .runner import replay_inputs, generate_new_inputs, run_echidna_campaign
+from .interface import extract_contract_bytecode
 from ..coverage import InstCoverage
+from ..common.logger import logger, handler
+import logging
+from typing import List, Set
 
 
 def main() -> None:
     args = parse_arguments()
+
+    if args.debug:
+        handler.setLevel(logging.DEBUG)
+    coverage_dir = os.path.join(args.corpus_dir, "coverage")
+
+    # Coverage tracker for the whole fuzzing session
     cov = InstCoverage()
-    # Replay echidna corpus
-    cov = replay_inputs(args.corpus_dir, args.contract, cov)
-    # Find inputs to reach new code
-    new_inputs = generate_new_inputs(cov)
-    print("New inputs:", new_inputs)
+    # Set of corpus files we have already processed
+    seen_files = set()
+
+    iter_cnt = 0
+    while args.max_iters is None or iter_cnt < args.max_iters:
+        iter_cnt += 1
+
+        # Run echidna fuzzing campaign
+        logger.info(f"Running echidna campaign #{iter_cnt} ...")
+        p = run_echidna_campaign(args)
+        if p.returncode != 0:
+            logger.fatal(f"Echidna failed with exit code {p.returncode}")
+            return
+
+        # Extract contract bytecodes in separate files for Maat. This is done
+        # only once after the first fuzzing campaign
+        if iter_cnt == 1:
+            # TODO(boyan): this should return a list of contracts if multiple contracts
+            # TODO(boyan): is it OK to assume crytic-export is always located in the
+            #       current working directory?
+            contract_file = extract_contract_bytecode("./crytic-export")
+
+        # Replay new corpus inputs symbolically
+        new_inputs = pull_new_corpus_files(coverage_dir, seen_files)
+        if new_inputs:
+            logger.info(
+                f"Echidna found {len(new_inputs)} new inputs. Replaying them symbolically..."
+            )
+        else:
+            logger.info(f"Echidna couldn't find new inputs")
+            return
+        cov = replay_inputs(new_inputs, contract_file, cov)
+
+        # Find inputs to reach new code
+        new_inputs_cnt = generate_new_inputs(cov)
+        if new_inputs_cnt > 0:
+            logger.info(f"Generated {new_inputs_cnt} new inputs")
+        else:
+            logger.info(f"Couldn't generate more inputs")
+            return
+
+
+def pull_new_corpus_files(cov_dir: str, seen_files: Set[str]) -> List[str]:
+    """Return files in 'cov_dir' that aren't present in 'seen_files'.
+    Before returning, 'seen_files' is updated to contain the list of new files
+    that the function returns
+    """
+    res = []
+    for corpus_file_name in os.listdir(cov_dir):
+        corpus_file = str(os.path.join(cov_dir, corpus_file_name))
+        if not corpus_file.endswith(".txt") or corpus_file in seen_files:
+            continue
+        else:
+            seen_files.add(corpus_file)
+            res.append(corpus_file)
+    return res
 
 
 def parse_arguments() -> argparse.Namespace:
 
     parser = argparse.ArgumentParser(
-        description="Concolic fuzzing tool",
+        description="Hybrid fuzzer with Echidna & Maat",
         prog=sys.argv[0],
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
+    def auto_int(x):
+        return int(x, 0)
+
+    # Echidna arguments
     parser.add_argument(
-        "corpus_dir", type=str, help="Echidna corpus directory to replay"
+        "FILES", type=str, nargs="*", help="Solidity files to analyze"
     )
 
     parser.add_argument(
-        "contract", type=str, help="Compiled smart contract to run"
+        "--corpus-dir",
+        type=str,
+        help="Directory to save and load corpus and coverage data",
+        metavar="PATH",
     )
+
+    parser.add_argument(
+        "--test-mode",
+        type=str,
+        help="Test mode to use",
+        choices=[
+            "property",
+            "assertion",
+            "dapptest",
+            "optimization",
+            "overflow",
+            "exploration",
+        ],
+        default="assertion",
+        metavar="MODE",
+    )
+
+    parser.add_argument(
+        "--seq-len",
+        type=int,
+        help="Number of transactions to generate during testing",
+        default=100,
+        metavar="INTEGER",
+    )
+
+    parser.add_argument(
+        "--config",
+        type=str,
+        help="Config file (command-line arguments override config options)",
+        metavar="FILE",
+    )
+
+    parser.add_argument(
+        "--test-limit",
+        type=int,
+        help="Number of sequences of transactions to generate",
+        default=50000,
+        metavar="INTEGER",
+    )
+
+    parser.add_argument(
+        "--contract-addr",
+        type=auto_int,
+        help="Address to deploy the contract to test",
+        default=0x00A329C0648769A73AFAC7F9381E08FB43DBEA72,
+        metavar="ADDRESS",
+    )
+
+    parser.add_argument(
+        "--deployer",
+        type=auto_int,
+        help="Address of the deployer of the contract to test",
+        default=0x0000000000000000000000000000000000030000,
+        metavar="ADDRESS",
+    )
+
+    parser.add_argument(
+        "--seed",
+        type=auto_int,
+        help="Run with a specific seed",
+        metavar="INTEGER",
+    )
+
+    # Optik arguments
+    parser.add_argument(
+        "--max-iters",
+        type=int,
+        help="Number of fuzzing campaigns to run. If unspecified, run until symbolic execution can't find new inputs",
+        default=None,
+        metavar="INTEGER",
+    )
+
+    parser.add_argument("--debug", action="store_true", help="Print debug logs")
 
     return parser.parse_args(sys.argv[1:])
 
--- optik/echidna/runner.py
+++ optik/echidna/runner.py
@@ -3,9 +3,11 @@ from ..coverage import InstCoverage
 from ..common.utils import symbolicate_tx_data
 from ..common.world import EVMWorld, WorldMonitor
 from ..common.logger import logger
+import argparse
+import subprocess
 import logging
 from maat import ARCH, contract, EVMTransaction, MaatEngine, Solver, STOP
-from typing import Optional
+from typing import List, Optional
 
 import os
 
@@ -24,7 +26,9 @@ tx_symbolicator = SymbolicateTxData()
 
 
 def replay_inputs(
-    corpus_dir: str, contract_file: str, cov: Optional[InstCoverage] = None
+    corpus_files: List[str],
+    contract_file: str,
+    cov: Optional[InstCoverage] = None,
 ) -> InstCoverage:
 
     # Building upon existing coverage?
@@ -32,23 +36,19 @@ def replay_inputs(
         cov = InstCoverage()
 
     # Run every input from the corpus
-    logger.info(f"Replaying inputs from corpus: {corpus_dir}")
-    for corpus_file_name in os.listdir(corpus_dir):
-        corpus_file = os.path.join(corpus_dir, corpus_file_name)
-        if not corpus_file.endswith(".txt"):
-            continue
-        logger.debug(f"Replaying input: {corpus_file_name}")
+    for corpus_file in corpus_files:
+        logger.debug(f"Replaying input: {os.path.basename(corpus_file)}")
 
-        tx = load_tx_sequence(corpus_file)[0]
+        tx_seq = load_tx_sequence(corpus_file)
         # TODO(boyan): implement snapshoting in EVMWorld so we don't
         # recreate the whole environment for every input
         world = EVMWorld()
-        world.deploy(contract_file, tx.recipient)
-        world.attach_monitor(cov, tx.recipient)
+        world.deploy(contract_file, tx_seq[0].recipient)
+        world.attach_monitor(cov, tx_seq[0].recipient)
         world.attach_monitor(tx_symbolicator)
 
         # Prepare to run transaction
-        world.push_transaction(tx)
+        world.push_transactions(tx_seq)
         cov.set_input_uid(corpus_file)
 
         # Run
@@ -57,17 +57,36 @@ def replay_inputs(
     return cov
 
 
-def generate_new_inputs(cov: InstCoverage):
+def generate_new_inputs(cov: InstCoverage) -> int:
+    """Generate new inputs to increase code coverage, base on
+    existing coverage
+
+    :param cov: coverage data
+    :return: number of new inputs found
+    """
 
     # Keep only interesting bifurcations
     cov.filter_bifurcations()
     cov.sort_bifurcations()
 
     res = []
+    # Count number of "unique" bifurcations, in the sense that
+    # two bifurcations to the same target are considered the same,
+    # even with different states and path constraints...
+    unique_targets = set()
+    for bif in cov.bifurcations:
+        if bif.alt_target not in unique_targets:
+            unique_targets.add(bif.alt_target)
     count = len(cov.bifurcations)
-    logger.info(f"Trying to solve {count} possible new paths...")
-
+    logger.info(
+        f"Solving potential new paths... ({count} total, {len(unique_targets)} unique)"
+    )
+    success_cnt = 0
     for i, bif in enumerate(cov.bifurcations):
+        # Don't solve the same branch target if it was solved already
+        if bif.alt_target not in unique_targets:
+            continue
+
         logger.info(f"Solving {i+1} of {count} ({round((i/count)*100, 2)}%)")
         s = Solver()
 
@@ -78,8 +97,39 @@ def generate_new_inputs(cov: InstCoverage):
         s.add(bif.alt_target_constraint)
 
         if s.check():
+            success_cnt += 1
+            unique_targets.remove(bif.alt_target)
             model = s.get_model()
             # Serialize the new input discovered
             store_new_tx_sequence(bif.input_uid, model)
 
-    return res
+    return success_cnt
+
+
+def run_echidna_campaign(
+    args: argparse.Namespace,
+) -> subprocess.CompletedProcess:
+    """Run an echidna fuzzing campaign
+
+    :param args: arguments to pass to echidna
+    :return: the exit value returned by invoking `echidna-test`
+    """
+    # Build back echidna command line
+    cmdline = ["echidna-test"]
+    cmdline += args.FILES
+    for arg, val in args.__dict__.items():
+        # Ignore Optik specific arguments
+        if arg not in ["FILES", "max_iters", "debug"] and not val is None:
+            cmdline += [f"--{arg.replace('_', '-')}", str(val)]
+    logger.debug(f"Echidna invocation cmdline: {' '.join(cmdline)}")
+    # Run echidna
+    echidna_process = subprocess.run(
+        cmdline,
+        # TODO(boyan): not piping stdout would allow to display the echidna
+        # interface while it runs, but we have to find a way to automatically
+        # terminate it with Ctrl+C or Esc once it finishes, otherwise it just
+        # hangs and the script can't continue
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    return echidna_process
