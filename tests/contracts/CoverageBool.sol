09b9c96355645ceb7b97f6167b67490e82cc2d65
Boyan-MILANOV <boyan.milanov@trailofbits.com>
2022-06-10 22:49:11 +0200

    Make boolean coverage test more sophisticated

--- tests/contracts/CoverageBool.sol
+++ tests/contracts/CoverageBool.sol
@@ -2,15 +2,22 @@ pragma solidity ^0.7.1;
 
 contract BoolArg {
 
-    function testBool(bool x, uint128 y) public returns (int16) {
-        if (y > 0x8000000 || x) {
-            if (y % 0x10000 == 0) {
+    function testBool(bool x, uint128 y, bool z) public returns (int16) {
+        if (y > 0x8000000 || x == z) {
+            if ((y % 0x10000 == 0) == z) {
                 return 3; // test::coverage
             }
 
             return 5; // test::coverage
         }
-
+        if (z || x){
+            if (x) {
+                return 1; // test::coverage
+            } else {
+                return 2; // test::coverage
+            }
+        }
+ 
         return 10; // test::coverage
     }
 }
