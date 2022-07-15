d3a043ac1c720b9efc9884298f99139caa2fdbbf
Tom <52238845+Thomas-Malcolm@users.noreply.github.com>
2022-07-15 23:33:13 +1000

    Optik Python Package (#48)
    * Python packaging requirements. GH action currently configured to publish to test repo, dependent on secret
    
    * Fixing GH action errors
    
    * GH workflow hot fix
    
    * Indentation problem with GH action
    
    * Further indentation and pypi publish version
    
    * Add option for manually running workflow for debugging purposes
    
    * on pushes to this branch run it
    
    * upload to pypi does not depend on legacy maat jobs
    
    * Upload package job couldn't reference dist/ directory because separate job. Rather than cache, we can compile them into the same job.
    
    * Fixing pymaat spelling
    
    Co-authored-by: Boyan MILANOV <boyan.milanov@trailofbits.com>
    
    * Separated into two jobs
    
    * Missing steps
    
    * pypi test link is out of date, updated
    
    * Minor changes
    
    * Install requirements in CI
    
    * Upload to real PyPI
    
    * Change triggers for packaging workflow
    
    Co-authored-by: Boyan MILANOV <boyan.milanov@trailofbits.com>

--- setup.py
+++ setup.py
@@ -1,32 +1,9 @@
 from setuptools import setup, find_packages
 
-version = "0.0.1"
-
-# Lint
-lint_deps = ["black~=22.0"]
-# Tests
-tests_deps = ["pytest"]
-
 # Setup
 setup(
-    name="optik",
-    description="TODO",
-    url="https://github.com/trailofbits/optik",
-    author="Trail of Bits",
-    version=version,
     packages=find_packages(exclude=["tests", "tests.*"]),
-    python_requires=">=3.7",
-    install_requires=[
-        "crytic-compile",
-        "pymaat>=0.6.2",
-        "eth_abi",
-        "pysha3",
-        "rlp"
-    ],
-    extras_require = {
-        "lint": lint_deps,
-        "tests": tests_deps,
-    },
+
     entry_points={
         "console_scripts": [
             "hybrid-echidna = optik.echidna.__main__:main"
